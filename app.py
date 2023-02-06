3db1b26d96bb28f0f64df785c250865f785055f1
bkaraba <bkaraba14@gmail.com>
2023-02-06 11:33:55 +0300

    remove initialization redudancy

--- app.py
+++ app.py
@@ -18,10 +18,6 @@ app.config.from_object(config_by_name["prod"])
 with app.app_context():
     db.app = app
     db.init_app(app)
-
-    db.create_all()
-    db.app = app
-    db.init_app(app)
     #db.create_all()
 
     JWTManager(app)
